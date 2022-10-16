37bb08f660be4255eda2b2b8b485de746a2bc397
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-16 09:54:37 -0400

    everything with current city today and forecast is displaying correctly, added Math(floor) to round down

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,10 @@ function getOneCallAPI(lat, lon) {
     })
     .then(function (data) {
       console.log("this is onecall API", data);
-      document.getElementById("temp").innerHTML = "Temp:  " + data.current.temp;
+      document.getElementById("temp").innerHTML =
+        "Temp:  " + Math.floor(data.current.temp);
       document.getElementById("wind_speed").innerHTML =
-        "Wind Speed:  " + data.current.wind_speed;
+        "Wind Speed:  " + Math.floor(data.current.wind_speed);
       document.getElementById("humidity").innerHTML =
         "Humidity:  " + data.current.humidity;
       var dateValue = moment.unix(data.current.dt).format("MM/DD/YYYY");
@@ -62,59 +63,54 @@ function getOneCallAPI(lat, lon) {
       document.getElementById("dateValue").innerHTML = dateValue;
 
       //forecast data - Day 1
-      console.log(data.daily[0]);
-      var date1 = moment.unix(data.daily[0].dt).format("MM/DD/YYYY");
+      console.log(data.daily[1]);
+      var date1 = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
       document.getElementById("date1").innerHTML = date1;
-
       document.getElementById("temp1").innerHTML =
-        "Temp:  " + data.daily[0].temp.day;
-      temp1(Math.round);
-
-      // $(".tempTomorrow").text(Math.round(data.daily[1].temp.day));
-
+        "Temp:  " + Math.floor(data.daily[1].temp.day);
       document.getElementById("wind_speed1").innerHTML =
-        "Wind Speed:  " + data.daily[1].wind_speed;
+        "Wind Speed:  " + Math.floor(data.daily[1].wind_speed);
       document.getElementById("humidity1").innerHTML =
         "Humidity:  " + data.daily[1].humidity;
 
       //Day 2
-      var date2 = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
+      var date2 = moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
 
       document.getElementById("date2").innerHTML = date2;
       document.getElementById("temp2").innerHTML =
-        "Temp:  " + data.daily[1].temp.day;
+        "Temp:  " + Math.floor(data.daily[2].temp.day);
 
       document.getElementById("wind_speed2").innerHTML =
-        "Wind Speed:  " + data.daily[2].wind_speed;
+        "Wind Speed:  " + Math.floor(data.daily[2].wind_speed);
       document.getElementById("humidity2").innerHTML =
         "Humidity:  " + data.daily[2].humidity;
 
       //Day 3
-      var date3 = moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
+      var date3 = moment.unix(data.daily[3].dt).format("MM/DD/YYYY");
       document.getElementById("date3").innerHTML = date3;
       document.getElementById("temp3").innerHTML =
-        "Temp:  " + data.daily[2].temp.day;
+        "Temp:  " + Math.floor(data.daily[2].temp.day);
       document.getElementById("wind_speed3").innerHTML =
-        "Wind Speed:  " + data.daily[3].wind_speed;
+        "Wind Speed:  " + Math.floor(data.daily[3].wind_speed);
       document.getElementById("humidity3").innerHTML =
         "Humidity:  " + data.daily[3].humidity;
 
       //Day 4
-      var date4 = moment.unix(data.daily[3].dt).format("MM/DD/YYYY");
+      var date4 = moment.unix(data.daily[4].dt).format("MM/DD/YYYY");
       document.getElementById("date4").innerHTML = date4;
       document.getElementById("temp4").innerHTML =
-        "Temp:  " + data.daily[3].temp.day;
+        "Temp:  " + Math.floor(data.daily[4].temp.day);
       document.getElementById("wind_speed4").innerHTML =
-        "Wind Speed:  " + data.daily[4].wind_speed;
+        "Wind Speed:  " + Math.floor(data.daily[4].wind_speed);
       document.getElementById("humidity4").innerHTML =
         "Humidity:  " + data.daily[4].humidity;
       //Day 5
-      var date5 = moment.unix(data.daily[4].dt).format("MM/DD/YYYY");
+      var date5 = moment.unix(data.daily[5].dt).format("MM/DD/YYYY");
       document.getElementById("date5").innerHTML = date5;
       document.getElementById("temp5").innerHTML =
-        "Temp:  " + data.daily[4].temp.day;
+        "Temp:  " + Math.floor(data.daily[5].temp.day);
       document.getElementById("wind_speed5").innerHTML =
-        "Wind Speed:  " + data.daily[5].wind_speed;
+        "Wind Speed:  " + Math.floor(data.daily[5].wind_speed);
       document.getElementById("humidity5").innerHTML =
         "Humidity:  " + data.daily[5].humidity;
     })
